a09a6a7259a18db641fefe3c219e0c94164a7438
Ryan Nowak <nowakra@gmail.com>
2019-12-16 21:05:15 -0800

    fixup

--- python/weatherapp-flask/gunicorn_conf.py
+++ python/weatherapp-flask/gunicorn_conf.py
@@ -3,7 +3,7 @@ import os
 
 workers = multiprocessing.cpu_count() * 3
 if "CPUS" in os.environ:
-  workers = int(os.environ["CPUS"]) * 3
+  workers = (int)(float(os.environ["CPUS"])) * 3
 
 bind = "0.0.0.0:5000"
 keepalive = 120
